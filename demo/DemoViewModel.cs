f723dc49d5f17cb82d0cf4f7c4ab5ebefa5c9309
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2023-12-26 16:29:23 -0500

    add light-dark theme toggle on-screen

--- demo/DemoViewModel.cs
+++ demo/DemoViewModel.cs
@@ -1,27 +1,64 @@
 ﻿using System.Collections.Generic;
 using System;
 using System.ComponentModel;
+using Avalonia.Styling;
 using Projektanker.Icons.Avalonia;
 
-namespace Demo;
-
-public class DemoViewModel : INotifyPropertyChanged
+namespace Demo
 {
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    private bool _isEnabled;
-    private IEnumerable<IconAnimation> _iconAnimations;
-
-    public bool IsEnabled
+    public class DemoViewModel : INotifyPropertyChanged
     {
-        get => _isEnabled;
-        set
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isEnabled;
+        private IEnumerable<IconAnimation> _iconAnimations;
+
+        public bool IsEnabled
         {
-            _isEnabled = value;
-            PropertyChanged?.Invoke(this, new(nameof(IsEnabled)));
+            get => _isEnabled;
+            set
+            {
+                _isEnabled = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+            }
+        }
+
+        private string _themeIcon;
+        public string ThemeIcon
+        {
+            get { return _themeIcon; }
+            set
+            {
+                if (_themeIcon != value)
+                {
+                    _themeIcon = value;
+                    OnPropertyChanged(nameof(ThemeIcon));
+                }
+            }
+        }
+
+        public IEnumerable<IconAnimation> Animations
+            => _iconAnimations ??= Enum.GetValues<IconAnimation>();
+
+        public DemoViewModel()
+        {
+            var app = Avalonia.Application.Current as App;
+            if (app != null)
+            {
+                if (app.RequestedThemeVariant == ThemeVariant.Light)
+                {
+                    ThemeIcon = "fa-solid fa-moon"; // Set icon to moon when light theme is selected
+                }
+                else
+                {
+                    ThemeIcon = "fa-solid fa-sun"; // Set icon to sun when dark theme is selected
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
-    public IEnumerable<IconAnimation> Animations
-        => _iconAnimations ??= Enum.GetValues<IconAnimation>();
 }
--- demo/MainWindow.axaml.cs
+++ demo/MainWindow.axaml.cs
@@ -2,6 +2,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Demo
 {
@@ -19,6 +20,24 @@ namespace Demo
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+        
+        public void ToggleTheme(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var app = Avalonia.Application.Current as Demo.App;
+            if (app != null)
+            {
+                if (app.RequestedThemeVariant == ThemeVariant.Light)
+                {
+                    app.RequestedThemeVariant = ThemeVariant.Dark;
+                    ((DemoViewModel)DataContext).ThemeIcon = "fa-solid fa-sun"; // Set icon to sun when dark theme is selected
+                }
+                else
+                {
+                    app.RequestedThemeVariant = ThemeVariant.Light;
+                    ((DemoViewModel)DataContext).ThemeIcon = "fa-solid fa-moon"; // Set icon to moon when light theme is selected
+                }
+            }
         }
     }
 }
